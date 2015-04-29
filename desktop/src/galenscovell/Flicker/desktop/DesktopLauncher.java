b3c439a8d47fe8b5c7b7f022388a7e4b0b029d34
Galen Scovell <galen.scovell@gmail.com>
2015-04-29 10:35:22 -0700

    Initial setup

---
+++ desktop/src/galenscovell/Flicker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package galenscovell.Flicker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import galenscovell.Flicker.FlickerMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlickerMain(), config);
+	}
+}
