2e9430099b1ff8559ebc2749d4169ad6ae7b397a
Galen Scovell <galen.scovell@gmail.com>
2015-05-07 17:37:08 -0700

    Set Table layout for Scene2d HUD

--- core/src/galenscovell/screens/HudDisplay.java
+++ core/src/galenscovell/screens/HudDisplay.java
@@ -7,26 +7,53 @@
 package galenscovell.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+
+import com.badlogic.gdx.utils.Align;
+import galenscovell.util.Constants;
 
 
 public class HudDisplay {
     private Stage stage;
-    private Table table;
-    private Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
 
 
     public HudDisplay() {
         this.stage = new Stage();
 
-        this.table = new Table();
-        Label testLabel = new Label("Testing", skin);
-        table.add(testLabel);
-        stage.addActor(table);
+        FreeTypeFontGenerator fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("ui/SDS_8x8.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 16;
+        BitmapFont retroFont = fontGenerator.generateFont(parameter);
+        fontGenerator.dispose();
+        Label.LabelStyle retroStyle = new Label.LabelStyle(retroFont, Color.WHITE);
+
+        Table mainTable = new Table();
+        mainTable.pad(30, 30, 600, 30);
+
+        Table healthTable = new Table();
+        healthTable.pad(0, 0, 0, 330);
+
+        Table inventoryTable = new Table();
+        inventoryTable.pad(0, 330, 0, 0);
+
+        Label healthLabel = new Label("HEALTH", retroStyle);
+        healthTable.add(healthLabel);
+        healthTable.pack();
+        mainTable.add(healthTable);
+
+        Label inventoryLabel = new Label("INVENTORY", retroStyle);
+        inventoryTable.add(inventoryLabel);
+        inventoryTable.pack();
+        mainTable.add(inventoryTable);
+
+        mainTable.pack();
+        stage.addActor(mainTable);
     }
 
     public void render() {
