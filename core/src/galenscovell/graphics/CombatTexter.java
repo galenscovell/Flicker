5f13cec0155e60f54a7bfeb29e7d959900577754
Galen Scovell <galen.scovell@gmail.com>
2015-11-29 20:30:23 -0800

    Implement combat text animation/alpha

--- core/src/galenscovell/graphics/CombatTexter.java
+++ core/src/galenscovell/graphics/CombatTexter.java
@@ -2,7 +2,6 @@ package galenscovell.graphics;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import galenscovell.things.entities.Entity;
-import galenscovell.util.Constants;
 
 import java.util.*;
 
@@ -16,13 +15,12 @@ public class CombatTexter {
     }
 
     public void addText(Entity entity, int val) {
-        int[] vals = new int[] {val, 0};
         displayed.add(new TextObject(entity, val));
     }
 
     public void draw(SpriteBatch batch) {
         for (TextObject obj : displayed) {
-            batch.draw(obj.getSprite(), obj.getX(), obj.getY(), Constants.TILESIZE / 2, Constants.TILESIZE / 2);
+            obj.draw(batch);
             if (obj.done()) {
                 finished.push(obj);
             }
--- core/src/galenscovell/graphics/TextObject.java
+++ core/src/galenscovell/graphics/TextObject.java
@@ -1,18 +1,19 @@
 package galenscovell.graphics;
 
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.*;
 import galenscovell.things.entities.Entity;
 import galenscovell.util.*;
 
 public class TextObject {
     private final Entity entity;
     public final Sprite sprite;
-    private int frame;
+    private int frame, size;
 
     public TextObject(Entity entity, int val) {
         this.entity = entity;
         this.sprite = ResourceManager.numbers[val];
-        this.frame = 0;
+        this.frame = 160;
+        this.size = Constants.TILESIZE / 2;
     }
 
     public Sprite getSprite() {
@@ -24,11 +25,29 @@ public class TextObject {
     }
 
     public float getY() {
-        return entity.getCurrentY() - Constants.TILESIZE;
+        if (frame > 100) {
+            return entity.getCurrentY() - ((1.0f - ((frame - 100) / 100.0f)) * Constants.TILESIZE);
+        } else {
+            return entity.getCurrentY() - Constants.TILESIZE;
+        }
+    }
+
+    public float getAlpha() {
+        if (frame > 30) {
+            return 1.0f;
+        } else {
+            return (frame / 30.0f);
+        }
     }
 
     public boolean done() {
-        frame++;
-        return frame == 120;
+        frame--;
+        return frame == 1;
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.setColor(1.0f, 1.0f, 1.0f, getAlpha());
+        batch.draw(sprite, getX(), getY(), size, size);
+        batch.setColor(1.0f, 1.0f, 1.0f, 1.0f);
     }
 }
