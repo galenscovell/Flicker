6617e2b6d34e9d47448c8b50c94347e7ab148bfb
Galen <galen.scovell@gmail.com>
2016-01-12 20:51:46 -0800

    Refactor/reorganize Pathfinder & Nodes

---
+++ core/src/galenscovell/Flicker/processing/Node.java
@@ -0,0 +1,38 @@
+package galenscovell.flicker.processing;
+
+import galenscovell.flicker.world.Tile;
+
+public class Node {
+    private Node parent;
+    private Tile tile;
+    private double cost;
+
+    public Node(Tile tile) {
+        this.tile = tile;
+        this.cost = Double.POSITIVE_INFINITY;
+    }
+
+    public Tile getTile() {
+        return tile;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public String toString() {
+        return "Node: Tile[" + getTile().x + ", " + getTile().y + "]";
+    }
+}
--- core/src/galenscovell/Flicker/processing/Pathfinder.java
+++ core/src/galenscovell/Flicker/processing/Pathfinder.java
@@ -6,59 +6,39 @@ import java.util.*;
 
 public class Pathfinder {
 
-    private static class Node {
-        Node parent;
-        Tile self;
-        double cost;
+    public Stack<Point> findPath(Tile startTile, Tile endTile, Repository repo) {
+        List<Node> openList = new ArrayList<Node>();
+        List<Node> closedList = new ArrayList<Node>();
+        Node startNode = new Node(startTile);
+        startNode.setCost(0);
+        Node endNode = new Node(endTile);
+        openList.add(startNode);
 
-        public Node(Tile s) {
-            this.self = s;
-            this.cost = Double.POSITIVE_INFINITY;
-        }
-    }
+        while (!openList.isEmpty()) {
+            Node current = getBestNode(openList, endNode);
 
-    public Stack<Point> findPath(Tile start, Tile end, Repository repo) {
-        List<Node> open = new ArrayList<Node>();
-        List<Tile> closed = new ArrayList<Tile>();
-        Node startNode = new Node(start);
-        startNode.cost = 0;
-        Node endNode = new Node(end);
-        open.add(startNode);
+            if (current.getTile() == endNode.getTile()) {
+                return tracePath(current);
+            }
 
-        while (!open.isEmpty()) {
-            // Consider node with best score in open list
-            Node a = chooseNode(open, endNode);
-            // If node tile is end tile, trace path and finish
-            if (a.self == end) {
-                return tracePath(a);
-            } else {
-                // Don't repeat ourselves
-                open.remove(a);
-                closed.add(a.self);
+            openList.remove(current);
+            closedList.add(current);
 
-                // Consider current node's neighbors
-                for (Point point : a.self.getNeighbors()) {
-                    Tile neighbor = repo.findTile(point.x, point.y);
-                    // Ignore walls, water and other blocked tiles
-                    if (neighbor == null || neighbor.isWater() || neighbor.isBlocking() || closed.contains(neighbor)) {
+            for (Point point : current.getTile().getNeighbors()) {
+                Tile neighborTile = repo.findTile(point.x, point.y);
+
+                if (neighborTile != null && !neighborTile.isWater() && !neighborTile.isBlocking()) {
+                    Node neighborNode = new Node(neighborTile);
+
+                    if (inList(neighborTile, openList) || inList(neighborTile, closedList)) {
                         continue;
-                    }
-                    // If tile is already in open list, ignore it
-                    boolean inOpen = false;
-                    for (Node n : open) {
-                        if (n.self == neighbor) {
-                            inOpen = true;
-                        }
-                    }
-                    Node adjacent = new Node(neighbor);
-                    // Otherwise add it as new unexplored node
-                    if (!inOpen) {
-                        open.add(adjacent);
+                    } else {
+                        openList.add(neighborNode);
                     }
                     // If this is a new path or shorter than current, keep it
-                    if (a.cost + 1 < adjacent.cost) {
-                        adjacent.parent = a;
-                        adjacent.cost = a.cost + 1;
+                    if (current.getCost() + 1 < neighborNode.getCost()) {
+                        neighborNode.setParent(current);
+                        neighborNode.setCost(current.getCost() + 1);
                     }
                 }
             }
@@ -66,36 +46,45 @@ public class Pathfinder {
         return null;
     }
 
-    private Node chooseNode(List<Node> open, Node end) {
+    private boolean inList(Tile nodeTile, List<Node> nodeList) {
+        for (Node node : nodeList) {
+            if (node.getTile() == nodeTile) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Node getBestNode(List<Node> openList, Node endNode) {
         double minCost = Double.POSITIVE_INFINITY;
         Node bestNode = null;
 
-        for (Node n : open) {
-            double costFromStart = n.cost;
-            double costToEnd = estimateDistance(n, end);
-            double totalCost = costFromStart + costToEnd;
-            if (minCost > totalCost) {
-                minCost = totalCost;
-                bestNode = n;
+        for (Node node : openList) {
+            double costFromStart = node.getCost();
+            double costToEnd = estimateDistance(node, endNode);
+            double totalCoat = costFromStart + costToEnd;
+            if (minCost > totalCoat) {
+                minCost = totalCoat;
+                bestNode = node;
             }
         }
         return bestNode;
     }
 
-    private double estimateDistance(Node n, Node end) {
-        // Euclidean Manhattan distance between nodes
-        double xs = (n.self.x - end.self.x) * (n.self.x - end.self.x);
-        double ys = (n.self.y - end.self.y) * (n.self.y - end.self.y);
+    private double estimateDistance(Node start, Node end) {
+        // Euclidean distance between nodes
+        double xs = (start.getTile().x - end.getTile().x) * (start.getTile().x - end.getTile().x);
+        double ys = (start.getTile().y - end.getTile().y) * (start.getTile().y - end.getTile().y);
         return Math.sqrt(xs + ys);
     }
 
-    private Stack<Point> tracePath(Node n) {
+    private Stack<Point> tracePath(Node node) {
         // Returns ordered stack of points along movement path
         Stack<Point> path = new Stack<Point>();
         // Chase parent of node until start point reached
-        while (n.parent != null) {
-            path.push(new Point(n.self.x, n.self.y));
-            n = n.parent;
+        while (node.getParent() != null) {
+            path.push(new Point(node.getTile().x, node.getTile().y));
+            node = node.getParent();
         }
         return path;
     }
